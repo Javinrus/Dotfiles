b9cd57709db2974a7b7fc786632248f5bb575080
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-09-06 19:42:21 +0700

    not done yet

--- .config/hypr/parts/binds.lua
+++ .config/hypr/parts/binds.lua
@@ -1,1 +1,6 @@
+-- https://wiki.hypr.land/Configuring/Basics/Binds/
+local overview = require("parts.functions")
 
+hl.bind("SUPER + Tab",     overview.next,  { submap_universal = true })
+hl.bind("SUPER + SUPER_L", overview.close, { release = true, transparent = true })
+hl.bind("SUPER + SUPER_R", overview.close, { release = true, transparent = true })
