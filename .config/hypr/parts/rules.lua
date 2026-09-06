317e84aaefe39c44f8164f064ca88eecdbce2499
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-09-06 18:37:34 +0700

    some cleaning and sematics
    Thunar is the best btw

--- .config/hypr/parts/rules.lua
+++ .config/hypr/parts/rules.lua
@@ -1,14 +1,11 @@
 -- https://wiki.hypr.land/Configuring/Basics/Window-Rules/
-
 hl.window_rule({
-    -- Ignore maximize requests from all apps
     name  = "Suppress maximize events",
     match = { class = ".*" },
     suppress_event = "maximize",
 })
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
     name  = "Fix XWayland drags",
     match = {
         class      = "^$",
@@ -18,7 +15,7 @@ hl.window_rule({
         fullscreen = false,
         pin        = false,
     },
-    
+
     no_focus = true,
 })
 
@@ -44,13 +41,14 @@ hl.window_rule({
     persistent_size = true,
 })
 
+-- Best file manager
 hl.window_rule({
     name = "Thunar dialogs",
     match = {
         class = "thunar|Thunar",
         title = 'Rename "*.*"|File Operation Progress|Confirm to replace files|Attention'
     },
-    
+
     float = true,
     persistent_size = true
 })
@@ -61,44 +59,16 @@ hl.window_rule({
         class = "thunar|Thunar",
         title = 'Attention|Rename "*.*"|Create Document from .*|New .* ...|Create New Folder'
     },
-    
-    stay_focused = true
-})
 
-hl.window_rule({
-    name = "Thunar move right bottom",
-    match = {
-        title = "^(File Operation Progress)$",
-        class = "(thunar|Thunar)",
-    },
-    
-    focus_on_activate = false,
-    move = "1460 970"
+    stay_focused = true
 })
 
 hl.window_rule({
     name = "Thunar menu force center",
     match = {
-        title = "^(Confirm to replace files)$",
         class = "(thunar|Thunar)"
+        title = "^(Confirm to replace files)$",
     },
-    
+
     center = true
-})
-
-hl.window_rule({
-    name = "Zen/firefox Picture in Picture",
-    match = {
-        title = "Picture-in-Picture",
-        class = "^(zen)(.*)$",
-    },
-
-    size = "300 200",
-    move = "1600 50",
-    border_size = 2,
-    pin = true,
-    float = true,
-    no_initial_focus = true,
-    focus_on_activate = true,
-    keep_aspect_ratio = true,
 })
