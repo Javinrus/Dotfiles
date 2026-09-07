cfd792140fb13831c8f719c06a89a48fcd427c04
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-09-07 08:38:50 +0700

    code cleaning and move plugin keys from binds.lua to plugins.lua

--- .config/hypr/parts/plugins.lua
+++ .config/hypr/parts/plugins.lua
@@ -1,27 +1,26 @@
 -- https://wiki.hypr.land/Plugins/Using-Plugins/
 hl.exec_cmd("hyprpm reload")
 
--- Prevents a temporary error from plugins not loaded yet
--- This happens because the config is evaluated before plugins are loaded
-if hl.plugin.scrolloverview ~= nil then
-    hl.config({
-        plugin = {
-            scrolloverview = {
-                gesture_distance = 300,
-                scale = 0.7,
-                workspace_gap = 30,
-                layout = "horizontal",
-                wallpaper = 0,
-                blur = false,
+hl.config({
+    plugin = {
+        scrolloverview = {
+            layout        = "horizontal",
+            scale         = 0.7,
+            workspace_gap = 30,
+            wallpaper     = 0,
 
-                shadow = {
-                    enabled = false,
-                },
+            blur = false,
+            shadow = {
+                enabled = false,
             },
-        },
-    })
 
-    hl.bind("SUPER + SHIFT + W", function()
-        hl.plugin.scrolloverview.overview("toggle all")
-    end)
-end
+            gesture_distance = 300,
+        },
+    },
+})
+
+local overview = require("parts.functions")
+
+hl.bind("SUPER + Tab",     overview.next,  { submap_universal = true })
+hl.bind("SUPER + SUPER_L", overview.close, { release = true, transparent = true })
+hl.bind("SUPER + SUPER_R", overview.close, { release = true, transparent = true })
