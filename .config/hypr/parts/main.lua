e29b9398e1df01c34bae6c33eab7cc7f84ffea0a
Javin Ruskyll <317805267+Javinrus@users.noreply.github.com>
2026-09-07 09:35:02 +0700

    Some configuring, also fix later: change color to Adamant + gradient
    Btw sorry no donate, maybe later when I have monehh :(

--- .config/hypr/parts/main.lua
+++ .config/hypr/parts/main.lua
@@ -1,23 +1,73 @@
---
+-- https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-    misc = {
-        animate_manual_resizes       = false,
-        animate_mouse_windowdragging = false,
+    general = {
+        gaps_in  = 5,
+        gaps_out = 10,
 
-        disable_hyprland_logo        = true,
-        force_default_wallpaper      = 0,
+        border_size      = 5,
+        resize_on_border = false,
 
-        on_focus_under_fullscreen    = 2,
-        allow_session_lock_restore   = true,
-        middle_click_paste           = false,
-        session_lock_xray            = false,
+        col = {
+            active_border   = "rgb(115, 121, 148)",
+            inactive_border = "rgb(35, 38, 52)",
+        },
 
+        -- Default layout to use
+        layout = "dwindle",
 
-        disable_splash_rendering     = true,
-        background_color             = "rgb(" .. theme.surfaceContainer .. ")", -- To be changed
+        allow_tearing = false,
     },
 
-    debug = {
-        error_position = 1
+    dwindle = {
+        preserve_split = true,
+    },
+
+    master = {
+        new_on_top = true,
+        new_status = "master",
+    },
+
+    scrolling = {
+        explicit_column_widths = "0.5, 0.6, 0.9, 1.0",
+        column_width           = 0.6,
+    },
+
+    decorations = {
+        rounding       = 5,
+        rounding_power = 5,
+
+        active_opacity   = 1.0,
+        inactive_opacity = 1.0,
+
+        blur = {
+           enabled = false,
+        },
+
+        shadow = {
+           enabled = false,
+        },
+    },
+
+    animations = {
+        enabled = true,
+    },
+
+    misc = {
+        force_default_wallpaper   = 0,
+        disable_hyprland_logo     = true,
+        disable_splash_rendering  = true,
+        focus_on_activate         = true,
+        on_focus_under_fullscreen = 0,
+        vrr                       = 2,
+    },
+
+    xwayland = {
+        enabled            = true,
+        force_zero_scaling = true,
+    },
+
+    ecosystem = {
+        no_update_news  = true,
+        no_donation_nag = true, -- Sorry :(
     }
 })
